6239d5f9c36f19ffcf5ee6c4e3df4896737381b9
sandmark <sandmark.m@gmail.com>
2019-02-26 20:10:25 +0900

    Define earth model

---
+++ my-cljapplied/src/my_cljapplied/ch1/modeling.clj
@@ -0,0 +1,9 @@
+(ns my-cljapplied.ch1.modeling)
+
+(def earth {:name       "Earth"
+            :moons      1
+            :volume     1.08321e12 ; km^3
+            :mass       5.97219e24 ; kg
+            :aphelion   152098232  ; km, farthest from sun
+            :perihelion 147098290  ; km, closest to sun
+            })
