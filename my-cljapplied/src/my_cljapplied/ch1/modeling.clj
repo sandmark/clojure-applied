aab4b88b473a654abe8c07f651c81dcea4bd437d
sandmark <sandmark.m@gmail.com>
2019-02-26 20:17:56 +0900

    Define instances of Planet in two ways

--- my-cljapplied/src/my_cljapplied/ch1/modeling.clj
+++ my-cljapplied/src/my_cljapplied/ch1/modeling.clj
@@ -1,5 +1,6 @@
 (ns my-cljapplied.ch1.modeling)
 
+;; A mapped earth
 (def earth {:name       "Earth"
             :moons      1
             :volume     1.08321e12 ; km^3
@@ -17,3 +18,18 @@
                    aphelion   ; km, farthest from sun
                    perihelion ; km, closest to sun
                    ])
+
+
+;; Positional factory function
+(def earth
+  (->Planet "Earth" 1 1.08321e12 5.97219e24 152098232 147098290))
+
+
+;; Map factory function
+(def earth
+  (map->Planet {:name       "Earth"
+                :moons      1
+                :volume     1.08321e12
+                :mass       5.97219e24
+                :aphelion   152098232
+                :perihelion 147098290}))
