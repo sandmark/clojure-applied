8024455260a8979db7da39ef5635743ac1771045
sandmark <sandmark.m@gmail.com>
2019-03-03 19:50:22 +0900

    Define a new Recipe instance

--- my-cljapplied/src/my_cljapplied/ch1/validate.clj
+++ my-cljapplied/src/my_cljapplied/ch1/validate.clj
@@ -6,3 +6,17 @@
 
 (defrecord Ingredient
     [name quantity unit])
+
+
+(def spaghetti-tacos
+  (map->Recipe
+   {:name        "Spaghetti Tacos"
+    :description "It's spaghetti... in a taco."
+    :ingredients [(->Ingredient "Spaghetti" 1 :lb)
+                  (->Ingredient "Spaghetti" 16 :oz)
+                  (->Ingredient "Taco shell" 12 :shell)]
+    :steps       ["Cook spaghetti according to box."
+                  "Heat spaghetti sauce until warm."
+                  "Mix spaghetti and sauce."
+                  "Put spaghetti in taco shells and serve."]
+    :servings    4}))
