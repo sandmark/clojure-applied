cc1b745d1646e1d1798f72a306882cb3abbcdf91
sandmark <sandmark.m@gmail.com>
2019-03-03 19:35:17 +0900

    Add record Recipe to learn Nesting Reference

---
+++ my-cljapplied/src/my_cljapplied/ch1/recipe.clj
@@ -0,0 +1,10 @@
+(ns my-cljapplied.ch1.recipe)
+
+(defrecord Recipe
+    [name        ; string
+     author      ; recipe
+     description ; stromg
+     ingredients ; list of ingredients
+     steps       ; sequence of string
+     servings    ; number of servings
+     ])
