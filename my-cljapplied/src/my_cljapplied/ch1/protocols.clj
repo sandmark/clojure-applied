5fd44e0c0358accf1392b063c2373963c546a523
sandmark <sandmark.m@gmail.com>
2019-03-05 07:59:11 +0900

    Extend protocols to protocols

--- my-cljapplied/src/my_cljapplied/ch1/protocols.clj
+++ my-cljapplied/src/my_cljapplied/ch1/protocols.clj
@@ -17,3 +17,21 @@
   Ingredient
   (cost [ingredient store]
     (cost-of store ingredient)))
+
+
+(defprotocol TaxedCost
+  (taxed-cost [entity store])
+  (tax-rate [store]))
+
+
+(extend-protocol TaxedCost
+  Object ;; default fallthrough
+  (taxed-cost [entity store]
+    (if (satisfies? Cost entity)
+      (do (extend-protocol TaxedCost
+            (class entity)
+            (taxed-cost [entity store]
+              (* (cost entity store)
+                 (inc (tax-rate store)))))
+          (taxed-cost entity store))
+      (assert false (str "Unhandled entity: " entity)))))
